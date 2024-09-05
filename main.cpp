83ee290cc16f01f1e904334724c45eb46af000f8
Encore <grisaia@inbox.ru>
2024-09-05 16:02:00 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+void print_logo();
+
+int main() {
+	print_logo();
+
+	return 0;
+}
+
+void print_logo() {
+	std::cout << "                     Develop by                       " << std::endl;
+	std::cout << " ######   ##  ##    ####     ####    #####    ######  \n"
+				 " ##       ### ##   ##  ##   ##  ##   ##  ##   ##		\n"
+				 " ##       ######   ##       ##  ##   ##  ##   ##		\n"
+				 " ####     ######   ##       ##  ##   #####    ####	\n"
+				 " ##       ## ###   ##       ##  ##   ####     ##		\n"
+				 " ##       ##  ##   ##  ##   ##  ##   ## ##    ##		\n"
+				 " ######   ##  ##    ####     ####    ##  ##   ######  " << std::endl;
+	std::cout << "            Programm for uchebnogo plana!" << std::endl;
+}
